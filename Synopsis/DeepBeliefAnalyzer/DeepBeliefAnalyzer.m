0e384f023055fbeb392e779062dcb1721315ca5f
vade <vade@vade.info>
2016-07-08 01:22:38 -0400

    Stabilize Deep Belief don't use Random

--- Synopsis/DeepBeliefAnalyzer/DeepBeliefAnalyzer.m
+++ Synopsis/DeepBeliefAnalyzer/DeepBeliefAnalyzer.m
@@ -125,7 +125,7 @@
         char** predictionsLabels;
         int predictionsLabelsLength;
         
-        jpcnn_classify_image(network, cnnInput, JPCNN_RANDOM_SAMPLE, 0, &predictions, &predictionsLength, &predictionsLabels, &predictionsLabelsLength);
+        jpcnn_classify_image(network, cnnInput, 0, 0, &predictions, &predictionsLength, &predictionsLabels, &predictionsLabelsLength);
 
         NSMutableDictionary* newValues = [NSMutableDictionary dictionary];
         for (int index = 0; index < predictionsLength; index += 1)
