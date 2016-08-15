d658dd4bb3f48113241857920b72b8ba08f74e36
vade <vade@vade.info>
2016-08-15 10:26:09 -0400

    Selection, work on animated sorting.

---
+++ Synopsis/Synopsis/AVASettingsTableViewCellController.h
@@ -0,0 +1,13 @@
+//
+//  AVASettingsTableViewCellController.h
+//  Synopsis
+//
+//  Created by vade on 8/14/16.
+//  Copyright © 2016 metavisual. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AVASettingsTableViewCellController : NSViewController
+
+@end
---
+++ Synopsis/Synopsis/AVASettingsTableViewCellController.m
@@ -0,0 +1,13 @@
+//
+//  AVASettingsTableViewCellController.m
+//  Synopsis
+//
+//  Created by vade on 8/14/16.
+//  Copyright © 2016 metavisual. All rights reserved.
+//
+
+#import "AVASettingsTableViewCellController.h"
+
+@implementation AVASettingsTableViewCellController
+
+@end
