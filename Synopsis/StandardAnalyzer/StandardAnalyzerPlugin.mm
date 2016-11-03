0e02357039f3046247b9ebbab32188607088fb02
vade <vade@vade.info>
2016-11-03 15:44:47 -0400

    Attempting to manually handle creation of devices for OpenCL Context

--- Synopsis/StandardAnalyzer/StandardAnalyzerPlugin.mm
+++ Synopsis/StandardAnalyzer/StandardAnalyzerPlugin.mm
@@ -89,6 +89,10 @@
 @property (atomic, readwrite, strong) NSMutableArray* everyDominantColor;
 @property (atomic, readwrite, strong) NSMutableArray* everyHash;
 
+@property (atomic, readwrite, assign) NSUInteger numIGPUCLDevices;
+@property (atomic, readwrite, assign) NSUInteger numDGPUCLDevices;
+@property (atomic, readwrite, assign) NSUInteger numCPUCLDevices;
+
 @end
 
 @implementation StandardAnalyzerPlugin
@@ -135,28 +139,34 @@
             if (!mainContext->create(cv::ocl::Device::TYPE_IGPU))
             {
                 NSLog(@"Unable to create Integrated GPU OpenCL Context");
-                
-                if (!mainContext->create(cv::ocl::Device::TYPE_DGPU))
-                {
-                    NSLog(@"Unable to create Discrete GPU OpenCL Context");
-                    
-                    if (!mainContext->create(cv::ocl::Device::TYPE_CPU))
-                    {
-                        NSLog(@"Unable to create CPU OpenCL Context");
-                    }
-                    else
-                    {
-                        NSLog(@"Created CPU OpenCL Context");
-                    }
-                }
-                else
-                {
-                    NSLog(@"Created Discrete GPU OpenCL Context");
-                }
             }
             else
             {
                 NSLog(@"Created Integrated GPU OpenCL Context");
+            }
+            
+            self.numIGPUCLDevices = mainContext->ndevices();
+            
+            if (!mainContext->create(cv::ocl::Device::TYPE_DGPU))
+            {
+                NSLog(@"Unable to create Discrete GPU OpenCL Context");
+            }
+            else
+            {
+                NSLog(@"Created Discrete GPU OpenCL Context");
+            }
+            
+            self.numDGPUCLDevices = mainContext->ndevices() - self.numIGPUCLDevices;
+
+    
+            if(!self.numIGPUCLDevices && !self.numDGPUCLDevices)
+            {
+                NSLog(@"No Integrated or Discreet OpenCL Devices exist, falling back to CPU Context");
+
+                if (!mainContext->create(cv::ocl::Device::TYPE_CPU))
+                {
+                    NSLog(@"Unable to create CPU OpenCL Context");
+                }
             }
             
             
